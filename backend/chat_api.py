abcc354ae5ea3aa0f7037435b8ea632207009a96
KoJieQi <122100368+KoJieQi@users.noreply.github.com>
2024-09-18 01:40:17 +0800

    Update chat_api.py

--- backend/chat_api.py
+++ backend/chat_api.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from knn import generate_response
 from chatbot import predict_chat
 
 app = Flask(__name__)
@@ -22,22 +21,6 @@ def convesation():
     }
 
     return jsonify(response), 201
-
-@app.route("/conversation2", methods=["POST"])
-def convesation():
-    # passed data
-    data = request.get_json().get("message")
-
-    prediction = generate_response(data)
-
-    print(data + " -> " + prediction)
-
-    response = {
-        "bot_response": prediction
-    }
-
-    return jsonify(response), 201
-
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=8888)
