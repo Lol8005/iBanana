07da665616ea74b15d0f6b418cb8065287ba4661
Lol8005 <ekhoos3141@gmail.com>
2024-09-19 01:37:46 +0800

    update

--- backend/accuracy_test.py
+++ backend/accuracy_test.py
@@ -1,19 +1,24 @@
 from chatbot import predict_chat
 import json
+import time
 
 with open('backend/dataset/intents.json', 'r',  encoding='utf-8') as f:
     intents = json.load(f)
 
 correct = total = 0
+start_pred = time.time()
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         (result, tag) = predict_chat(pattern) 
         if tag == intent["tag"]:
             correct += 1
+
+            print(f"")
         else:
             print(f"Incorrect: {tag} -> Correct: {intent["tag"]}")
 
         total += 1
 
 print(f"Bot accuracy: {round(correct / total * 100, 2)}%")
+print(f"Total Prediction time: {time.time() - start_pred:} seconds")
--- standalone-frontend/index.php
+++ standalone-frontend/index.php
@@ -101,8 +101,8 @@ include 'product_cardgroup/productClass.php';
         }
     </style>
 
-    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
